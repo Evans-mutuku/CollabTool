a69e91fdbc074ecb1e37f8228b121604ed89ef14
Evans-mutuku <evansnyamai98@gmail.com>
2024-05-20 22:22:46 +0300

    creating colabtool

--- server/routes/docRoutes.js
+++ server/routes/docRoutes.js
@@ -1,9 +1,13 @@
-// server/routes/authRoutes.js
+// server/routes/docRoutes.js
 const express = require("express");
 const router = express.Router();
-const { register, login } = require("../controllers/authController");
+const {
+  createDocument,
+  getDocuments,
+} = require("../controllers/docController");
+const auth = require("../middleware/auth");
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/documents", auth, createDocument);
+router.get("/documents", auth, getDocuments);
 
 module.exports = router;
