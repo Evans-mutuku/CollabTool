731ee0644edb2f37a7c0aea91d68a455c2ca3d36
Evans-mutuku <evansnyamai98@gmail.com>
2024-05-23 11:41:12 +0300

    working with  advanced courses

--- server/routes/docRoutes.js
+++ server/routes/docRoutes.js
@@ -1,11 +1,26 @@
 // server/routes/docRoutes.js
 const express = require("express");
 const router = express.Router();
+const Document = require("../models/Document");
+
 const {
   createDocument,
   getDocuments,
 } = require("../countrollers/docController");
 const auth = require("../middleware/auth");
+
+// Create a new document
+router.post("/create", async (req, res) => {
+  const { title } = req.body;
+  const newDocument = new Document({ title, content: "" });
+  await newDocument.save();
+  res.status(201).json(newDocument);
+});
+// Get all documents
+router.get("/", async (req, res) => {
+  const documents = await Document.find({});
+  res.json(documents);
+});
 
 router.post("/documents", auth, createDocument);
 router.get("/documents", auth, getDocuments);
--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-// server/server.js
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
@@ -8,6 +7,7 @@ const docRoutes = require("./routes/docRoutes");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const path = require("path");
+const Document = require("./models/Document"); // Import the Document model
 
 dotenv.config();
 
@@ -19,25 +19,59 @@ connectDB();
 
 app.use(bodyParser.json());
 
+// Serve static files
+app.use(express.static(path.join(__dirname, "../client")));
+
 app.use("/api/auth", authRoutes);
 app.use("/api/docs", docRoutes);
 
-app.use(express.static(path.join(__dirname, "../client")));
-
+// Route to serve index.html for root URL
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/index.html"));
 });
+
 io.on("connection", (socket) => {
   console.log("New client connected");
 
-  socket.on("join", (docId) => {
+  socket.on("join", async (docId) => {
     socket.join(docId);
     console.log(`Client joined room: ${docId}`);
+
+    // Load the document content from the database
+    const document = await Document.findById(docId);
+    if (document) {
+      socket.emit("receiveEdit", document.content);
+    } else {
+      // If the document does not exist, create a new one
+      const newDocument = new Document({
+        _id: docId,
+        content: "",
+        title: `Document ${docId}`,
+      });
+      await newDocument.save();
+      socket.emit("receiveEdit", newDocument.content);
+    }
   });
 
-  socket.on("edit", (data) => {
+  socket.on("edit", async (data) => {
     const { docId, content } = data;
-    io.to(docId).emit("receiveEdit", content);
+
+    // Save or update the document content in the database
+    const document = await Document.findById(docId);
+    if (document) {
+      document.content = content;
+      document.versionHistory.push({ content });
+      await document.save();
+    } else {
+      await Document.findByIdAndUpdate(
+        docId,
+        { content, $push: { versionHistory: { content } } },
+        { upsert: true }
+      );
+    }
+
+    // Broadcast the changes to other clients in the same room
+    socket.to(docId).emit("receiveEdit", content);
   });
 
   socket.on("disconnect", () => {
